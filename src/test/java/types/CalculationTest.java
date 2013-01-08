1478469ff42f38da8498599e1a37a46e9340689f
Akira Shiromizu <Akira.Shiromizu@sptgroup.com>
2013-01-08 16:02:29 +0100

    Test fix and added MAVEN dependency to the project

--- src/test/java/types/CalculationTest.java
+++ src/test/java/types/CalculationTest.java
@@ -16,12 +16,17 @@ public class CalculationTest {
 	@Test
 	public void testCalculation() {
 		List<Transaction> transactions = new ArrayList<Transaction>();
+
+		transactions.add(new Transaction(new Amount(200, Currency.NOK), Type.Supermarket, new Date(System.currentTimeMillis()-648000), User.Akira));
+		transactions.add(new Transaction(new Amount(400, Currency.SEK), Type.Groceries, new Date(System.currentTimeMillis()-548000), User.Sara));
+		
 		for (int i = 0; i < 10; i++)
 			transactions.add(new Transaction(new Amount(10, Currency.NOK), Type.Supermarket, new Date(System.currentTimeMillis()), User.Akira));
 		
+		
 		Calculations impl = new Calculations();
 		double actualsuper = impl.getTotal(transactions, Type.Supermarket);
-		int expectedsuper = 10 * 10;
+		int expectedsuper = (10 * 10) + 200;
 		
 		for (int i = 10; i < 20; i++)
 			transactions.add(new Transaction(new Amount(25, Currency.NOK), Type.Food, new Date(System.currentTimeMillis()), User.Akira));
